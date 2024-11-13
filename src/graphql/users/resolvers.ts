7ae113d5e354daf9801369287c1da0d7d4e0ac93
Jacobo Ossa <jaco2419@gmail.com>
2024-11-12 20:43:11 -0500

    Add graphql folder

---
+++ src/graphql/users/resolvers.ts
@@ -0,0 +1,34 @@
+import { UserDocument, UserInput } from "../../model/user.model";
+import userService from "../../services/user.service";
+
+export const resolvers = {
+    Query: {
+        user: async (_root: any, params: any) => {
+            console.log(params.id);
+            const user: UserDocument | null = await userService.findByEmail(params.id);
+            return user;
+        }, 
+        users: (_root: any) => userService.findAll(),
+
+    },
+    Mutation: {
+        createUser: async (_root: any, params: any )=> {
+            const userOutput: UserDocument = await userService.createUser(params.input as UserInput);
+            return userOutput;
+        },
+        updateUser: async (_root: any, params: any) => {
+            const userOutput: UserDocument | null = await userService.update(params.input.id, params.input as UserInput);
+            return userOutput;
+        },
+        deleteUser: async (_root: any, params: any) => {
+            const userOutput: UserDocument | null = await userService.delete(params.input.id);
+            return userOutput;
+        }
+    
+    }
+
+
+
+
+
+}
